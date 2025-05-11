bf5c34dde18b1b9af42a6337e26e88dea4f48b9c
Darine Abdelmotalib <darine.motalib@gmail.com>
2025-05-11 11:36:53 -0400

    switched to hashrouter for github pages

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,15 @@
 
 import './App.scss'
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { HashRouter, Routes, Route } from 'react-router-dom';
 import HomePage from "./pages/HomePage/HomePage";
 
 function App() {
   return (
-    <BrowserRouter>
+    <HashRouter>
       <Routes>
         <Route path="/" element={<HomePage />}></Route>
       </Routes>
-    </BrowserRouter>
+    </HashRouter>
   )
 }
 
